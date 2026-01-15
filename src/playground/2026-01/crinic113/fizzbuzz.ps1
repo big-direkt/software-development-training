054819bbe590dc544fcc758592d6639d7c1ca718
Cristian Niculae <cristian.niculae@big-direkt.de>
2026-01-15 11:16:34 +0100

    my fizzbuzz project (copied)

---
+++ src/playground/2026-01/crinic113/fizzbuzz.ps1
@@ -0,0 +1,16 @@
+for ($i = 1; $i -le 20; $i++) {
+    # Zuerst auf Teilbarkeit durch 3 und 5 prüfen,
+    # da diese Bedingung spezieller ist als die einzelnen Teilbarkeiten.
+    if (($i % 3 -eq 0) -and ($i % 5 -eq 0)) {
+        Write-Host "FizzBuzz"
+    }
+    elseif ($i % 3 -eq 0) {
+        Write-Host "Fizz"
+    }
+    elseif ($i % 5 -eq 0) {
+        Write-Host "Buzz"
+    }
+    else {
+        Write-Host $i
+    }
+}
